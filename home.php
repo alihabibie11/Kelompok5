c2361ced8b5f1a636aafc880c5b489f5043146e8
Alihabibie11 <malizzulhaq08@gmail.com>
2019-11-13 10:44:34 +0700

    Add photo on Gallery..

---
+++ home.php
@@ -0,0 +1,16 @@
+<html>
+<head></head>
+<body>
+<table width="100%" border="1">
+<tr>
+<td colspan="3" align="center"><h3><b>My Website</b></h3></td>
+</tr>
+<tr>
+<td align="center"><a href="home.php"><b>Home</b></a></td>
+<td align="center"><a href="Profile.php"><b>Profile</b></a></td>
+<td align="center"><a href="Contact.php"><b>Contact</b></a></td>
+</tr>
+</table>
+<h2>Halaman Home</h2>
+</body>
+</html>
